562d238d5f9540328abf7a45d096bfb72a6cf12d
kira076 <j.morrisoncleary@gmail.com>
2021-01-05 21:33:13 +1100

    Update datahelper.py

--- extensions/helpers/datahelper.py
+++ extensions/helpers/datahelper.py
@@ -12,9 +12,7 @@ import keyring
 class DataHelper(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
-    dbkey = quote_plus(keyring.get_password("system", "AdminKira"))
-    dburi = "mongodb://AdminKira:" + dbkey + "@localhost:27017"
-    del dbkey
+    dburi = "mongodb://AdminKira:" + quote_plus(bytes(keyring.get_password("system", "AdminKira"))) + "@localhost:27017"
     self.mongo = motor.motor_asyncio.AsyncIOMotorClient(dburi)
 
     p = Path('/home/kira/kml/KMLegion-Bot/responses.json')
