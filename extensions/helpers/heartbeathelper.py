763fd9e7612bcb6a8351a78188005076d258154e
kira076 <j.morrisoncleary@gmail.com>
2021-01-07 19:22:10 +1100

    Update heartbeathelper.py

--- extensions/helpers/heartbeathelper.py
+++ extensions/helpers/heartbeathelper.py
@@ -63,7 +63,7 @@ class HeartbeatHelper(commands.Cog):
       await ctx.send('Heartbeat is already running!')
 
   async def _check_sync(self):
-      response = await self.ntp.request('uk.pool.ntp.org')
+      response = self.ntp.request('uk.pool.ntp.org')
       print(ctime(response.tx_time))
 
 def setup(bot):
